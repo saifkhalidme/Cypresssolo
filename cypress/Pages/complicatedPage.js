9c50e05fbe7ff3ac6ad14f384388784f32e0ed38
RacLab <saifkhalid.me@gmail.com>
2026-01-01 21:38:04 +0500

    this is latest

--- cypress/Pages/complicatedPage.js
+++ cypress/Pages/complicatedPage.js
@@ -4,7 +4,7 @@ basicDetail(){
 
 cy.get('#name').type("saif")
 cy.get('#email').type("testemail@gmail.com")
-cy.get('[placeholder=Enter "Phone"]').type("03248743284")
+cy.get('[placeholder="Enter Phone"]').type("03248743284")
 cy.get('#textarea').type("DHA PH8 W block CC4 Lahore")
 }
 }
@@ -18,6 +18,7 @@ cy.get('#country').select('china')
 cy.get ('#colors').select('Green')
 }
 }
+
 class DatesSubmit{
 
   dateSelector(){
@@ -25,10 +26,30 @@ class DatesSubmit{
   cy.get('#datepicker').click()                         
   cy.get('.ui-datepicker-calendar').should('be.visible')
   cy.get('.ui-datepicker-calendar').contains('2').click()
+ 
+  }
+  }
+
+  class Pictures{
+
+    imageFiless(){  
+  cy.get('#singleFileInput')
+  cy.get('input[type="file"]').eq(0).selectFile('cypress/fixtures/picone.png',{ force: true })
+  cy.get('button[type="submit"]').eq(0).click()
+  }
+  }
   
+  class SearchBar{
+    searchWeb(){
+    cy.get('#Wikipedia1_wikipedia-search-input').type("Razor1")
+    cy.get('.wikipedia-search-button').should('be.visible').click()
+    cy.get('a[href="https://en.wikipedia.org/wiki/Razor_1911"]').invoke('removeAttr','target').click()
+    cy.go('back')
+   
+  }
   
-}
-}
+  }
 
 
-export default {FormFill,PersonalDetail,DatesSubmit}
+
+export default {FormFill,PersonalDetail,DatesSubmit,Pictures,SearchBar}
--- cypress/e2e/formfill.cy.js
+++ cypress/e2e/formfill.cy.js
@@ -1,25 +1,33 @@
-import {FormFill,PersonalDetail,DatesSubmit} from '../Pages/complicatedPage'
+import {FormFill,PersonalDetail,DatesSubmit,Pictures,SearchBar} from '../Pages/complicatedPage'
 
 const form1= new FormFill()
 const form2= new PersonalDetail()
 const date1= new DatesSubmit()
+const image1= new Pictures()
+const searchit= new SearchBar()
 
 describe('testingforms', ()=>{
 
- it('first test' , ()=> {
+ it('basics' , ()=> {
     cy.webVisit()
-    form1.basicDetail
+    form1.basicDetail()
  })
 
- it('second test' , ()=>{
+ it('personal detail' , ()=>{
     cy.webVisit()
     form2.pDetail() 
 })
 
- it ('date' , ()=>{
+ it ('date entry' , ()=>{
     cy.webVisit()
     date1.dateSelector() 
-   
-})
-
+ })
+ it ('image upload', ()=>{
+   cy.webVisit()
+   image1.imageFiless()
+ })  
+ it ('websearch' , ()=>{
+    cy.webVisit()
+    searchit.searchWeb()
+ })
 })
