1bf50e98d86bc4ea7c5189fba0872c73ac1e9e06
RacLab <saifkhalid.me@gmail.com>
2026-01-06 13:30:11 +0500

    none

--- cypress.config.js
+++ cypress.config.js
@@ -1,14 +1,20 @@
 const { defineConfig } = require("cypress");
-const dotenv = require("dotenv");
 
-dotenv.config({ path: ".env.orangehrmlogin" });
 module.exports = defineConfig({
   e2e: {
     execTimeout: 70000,
-     
+
     setupNodeEvents(on, config) {
-     baseUrl = process.env.CYPRESS_BASE_URL;
-     return config;
+      // no env configuration
+      return config;
     },
+  },
+
+  reporter: "mochawesome",
+  reporterOptions: {
+    reportDir: "cypress/reports",
+    overwrite: false,
+    html: false,
+    json: true,
   },
 });
