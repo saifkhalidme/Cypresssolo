bf9d18e81d062b3007ef9ba5dce06adf0b0b4ea3
RacLab <saifkhalid.me@gmail.com>
2026-01-20 19:26:20 +0500

    sidepanel

---
+++ cypress/selectors/Toolsqa/sidepanelselectors.js
@@ -0,0 +1,12 @@
+class SidePanel {
+
+    leftSidePanel = '.left-pannel'
+    firstOption = '.header-text'
+    secondOption = '.header-text'
+    thrirdOption = '.header-text'
+    dropdownbutton='.icon'
+    dropdownOptions = '.menu-list'
+
+}
+
+export default SidePanel 
